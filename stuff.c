fccbe01249a74e393958a8591046495b93811865
comex <comexk@gmail.com>
2010-07-25 16:54:43 -0400

    more sensible

--- stuff.c
+++ stuff.c
@@ -14,6 +14,57 @@ int copy_phys(uint32_t paddr, uint32_t size, void *buf) {
     return syscall(8, 1, paddr, size, buf);
 }
 
+static const char *cacheable(uint32_t flags) {
+    const char *descs[] = {
+        "Non-cacheable",
+        "Write-Back, Write-Allocate",
+        "Write-Through, no Write-Allocate",
+        "Write-Back, no Write-Allocate"
+    };
+    return descs[flags];
+}
+
+static const char *tex(uint32_t tex, uint32_t c, uint32_t b) {
+    if(tex & 4) {
+        static char buf[1024];
+        sprintf(buf, "Cacheable, Outer:%s, Inner:%s", cacheable(tex & 3), cacheable(((c & 1) << 1) | (b & 1)));
+        return buf;
+    }
+
+    const char *descs[] = {
+        "Strongly-ordered",
+        "Shareable Device",
+        "Outer and Inner Write-Through, no Write-Allocate",
+        "Outer and Inner Write-Back, no Write-Allocate",
+        "Outer and Inner Non-cacheable",
+        "Reserved! 00101",
+        "Implementation defined! 00110",
+        "Outer and Inner Write-Back, Write-Allocate",
+        "Non-shareable Device",
+        "Reserved! 01001",
+        "Reserved! 01010",
+        "Reserved! 01011",
+    };
+    uint32_t thing = ((tex & 7) << 2) |
+                     ((c & 1) << 1) |
+                     (b & 1);
+    return descs[thing];
+}
+
+static const char *ap(uint32_t ap) {
+    const char *descs[] = {
+        "X/X",
+        "RW/X",
+        "RW/RO",
+        "RW/RW",
+        "100!",
+        "RO/X",
+        "RO/RO",
+        "111!",
+    };
+    return descs[ap & 3];
+}
+
 static void dump_pagetable(uint32_t ttbr, uint32_t baseaddr, uint32_t size) {
     unsigned int *data = malloc(size);
     assert(!copy_phys(ttbr & ~0x3f, size, data));
@@ -23,47 +74,61 @@ static void dump_pagetable(uint32_t ttbr, uint32_t baseaddr, uint32_t size) {
         printf("%08x: ", baseaddr + i * 0x100000);
         switch(l1desc & 3) {
         case 1: {
-            printf("coarse page table base=%x P=%d domain=%d\n", l1desc & ~0x3ff, (l1desc & (1 << 9)) ? 1 : 0, (l1desc >> 5) & 0xf);
+            printf("coarse page table base=%x P=%d domain=%d\n", l1desc & ~0x3ff, (l1desc & (1 << 9)) & 1, (l1desc >> 5) & 0xf);
             unsigned int data2[256];
             memset(data2, 0xff, sizeof(data2));
             assert(!copy_phys(l1desc & ~0x3ff, sizeof(data2), data2));
             for(int j = 0; j < 256; j++) {
-                unsigned int l2desc = data2[i];
+                unsigned int l2desc = data2[j];
                 if((l2desc & 3) == 0) continue; // fault
                 printf("  %08x: ", baseaddr + (i * 0x100000) + (j * 0x1000));
                 switch(l2desc & 3) {
                 case 1:
-                    printf("large base=%x TEX=%d AP0=%d AP1=%d AP2=%d AP3=%d C=%d B=%d\n", l2desc & 0xffff0000, (l2desc >> 12) & 7, (l2desc >> 4) & 3, (l2desc >> 6) & 3, (l2desc >> 8) & 3, (l2desc >> 10) & 3, (l2desc >> 3) & 1, (l2desc >> 2) & 1);
+                    printf("large base=%x [%s] AP0=%s AP1=%s AP2=%s AP3=%s\n",
+                        l2desc & 0xffff0000,
+                        tex(l2desc >> 12, l2desc >> 3, l2desc >> 2),
+                        ap(l2desc >> 4),
+                        ap(l2desc >> 6),
+                        ap(l2desc >> 8),
+                        ap(l2desc >> 10));
                     break;
                 case 2:
-                    printf("large base=%x AP0=%d AP1=%d AP2=%d AP3=%d C=%d B=%d\n", l2desc & 0xfffff000, (l2desc >> 4) & 3, (l2desc >> 6) & 3, (l2desc >> 8) & 3, (l2desc >> 10) & 3, (l2desc >> 3) & 1, (l2desc >> 2) & 1);
+                    printf("small base=%x AP0=%s AP1=%s AP2=%s AP3=%s C=%d B=%d\n",
+                        l2desc & 0xfffff000,
+                        ap(l2desc >> 4),
+                        ap(l2desc >> 6),
+                        ap(l2desc >> 8),
+                        ap(l2desc >> 10),
+                        (l2desc >> 3) & 1,
+                        (l2desc >> 2) & 1);
                     break;
                 case 3:
-                    printf("reserved (probably; extended small page)\n");
+                    printf("extended small base=%x [%s] AP=%s\n",
+                        l2desc & 0xfffff00,
+                        tex(l2desc >> 6, l2desc >> 3, l2desc >> 2),
+                        ap(l2desc >> 4));
                     break;
                 }
             }
             } break;
         case 2:
             if(l1desc & (1 << 18)) {
-                printf("supersection base=%x extbase=%x NS=%d nG=%d TEX=%x AP=%x extbase2=%x XN=%x C=%x B=%x\n",
+                printf("supersection base=%x extbase=%x NS=%d nG=%d [%s] AP=%s extbase2=%x XN=%x\n",
                     l1desc & 0xff000000,
                     (l1desc >> 20) & 0xf,
-                    (l1desc >> 19) ? 1 : 0,
-                    (l1desc >> 17) ? 1 : 0,
-                    (l1desc >> 12) & 7,
-                    ((l1desc >> 15) ? 4 : 0) | ((l1desc >> 10) & 3),
+                    (l1desc >> 19) & 1,
+                    (l1desc >> 17) & 1,
+                    tex(l1desc >> 12, l1desc >> 3, l1desc >> 2),
+                    ap(((l1desc >> 15) ? 4 : 0) | ((l1desc >> 10) & 3)),
                     (l1desc >> 5) & 0xf,
-                    (l1desc >> 4) ? 1 : 0,
-                    (l1desc >> 3) ? 1 : 0,
-                    (l1desc >> 2) ? 1 : 0);
+                    (l1desc >> 4) & 1);
 
             } else {
                 printf("section!!\n");
             }
             break;
         case 3:
-            printf("reserved\n");
+            printf("fine page table!!\n");
             break;
         }
     }
