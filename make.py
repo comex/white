060d198eaa0787a68d60f964f7e0f757692c1847
comex <comexk@gmail.com>
2010-07-25 14:41:07 -0400

    make.py works

--- make.py
+++ make.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python
 from fabricate import *
 sources = ['kcode.c', 'kasm.S']
 def build():
-    run('gcc-4.2 -o loader loader.c -arch armv6 -isysroot /var/sdk')
-    run('ldid -S loader')
+    run('gcc-4.2', '-o', 'loader', 'loader.c', '-arch', 'armv6', '-isysroot', '/var/sdk')
+    #run('ldid', '-S', 'loader')
 
-    run('python nm.py')
+    run('python', 'nm.py')
 
-    GCC = 'arm-none-eabi-gcc  -mthumb -march=armv7 -Os'
+    GCC = ['arm-none-eabi-gcc', '-mthumb', '-march=armv7', '-Os']
     OBJCOPY = 'arm-none-eabi-objcopy'
     for source in sources:
         run(GCC, '-c', source)
